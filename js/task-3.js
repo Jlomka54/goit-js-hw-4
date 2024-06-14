af5fbeb98fad9cf368e00c16ea1122d3cc934eb3
Bohdan Haponiuk <jlomka54@gmail.com>
2024-06-14 16:22:41 +0400

    add task

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,24 @@
-function filterArray(numbers, value) {
-    const numArray = [];
-    for (const number of numbers) {
-        if (number > value) {
-            numArray.push(number);
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername(newName) {
+        if (this.username !== newName) {
+           this.username = newName;
         }
-    }
-    return numArray;
-}
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]~
+     },
+    updatePlayTime(hours) {
+        if (hours > 0) {
+            this.playTime += hours;
+        }
+     },
+        getInfo() {
+            return `${this.username} has ${this.playTime} active hours!`
+        }
+};
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
